03ef00c97462caf0dc44b370c8f7be122ce74c2d
Vivian La Farina <vivianlafarina@gmail.com>
2023-06-20 21:17:49 -0500

    adding • Middlewares • Express-validators • Variables de entorno • Autenticación y autorización :art:

---
+++ src/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const app = require('./app');
+const { db } = require('./database/config');
+const initModel = require('./models/initModels');
+
+db.authenticate()
+  .then(() => console.log('Database authenticated ..👽'))
+  .catch((err) => console.log(err));
+
+initModel();
+
+db.sync()
+  .then(() => console.log('Database synchronized...🤞🏻'))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}!...🦾👾`);
+});
